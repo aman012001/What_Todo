9cda66178473bea330966207110bca7f1bc41b9c
aman012001 <amanneeeraj@gmail.com>
2020-09-06 22:09:48 +0530

    Made UI for the Create Task Screen

--- lib/screens/HomePage_Screen.dart
+++ lib/screens/HomePage_Screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todo/screens/Task_Screen.dart';
 import 'package:todo/widgets.dart';
 
 class Homepage extends StatefulWidget {
@@ -18,31 +19,47 @@ class _HomepageState extends State<Homepage> {
             padding: EdgeInsets.all(24.0),
             child: Stack(
               children: [
-                Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Image(
-                    image: AssetImage(
-                        'assests/images/logo.png'
-                    ),
-                   ),
-                    TaskCard(),
-                    TaskCard(),
-                  ]
-              ),
+                Expanded(
+                  child: ListView(
+                    children: [ Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image(
+                        image: AssetImage(
+                            'assests/images/logo.png'
+                        ),
+                       ),
+                        TaskCard(),
+                        TaskCard(),
+                        TaskCard(),
+                        TaskCard(),
+                        TaskCard(),
+                        TaskCard(),
+                      ]
+                     ),
+                    ]
+                  ),
+                ),
                 Positioned(
-                  bottom: 0.0,
+                  bottom: 24.0,
                   right: 0.0,
-                  child: Container(
-                    width: 50.0,
-                    height: 50.0,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.0),
-                      color: Color(0xff7349fe),
-                    ),
-                    child: Image(
-                      image: AssetImage(
-                        "assests/images/add_icon.png",
+                  child: GestureDetector(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context)=>Taskpage(),
+                      ));
+                    },
+                    child: Container(
+                      width: 50.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Color(0xff7349fe),
+                      ),
+                      child: Image(
+                        image: AssetImage(
+                          "assests/images/add_icon.png",
+                        ),
                       ),
                     ),
                   ),
